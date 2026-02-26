5aaca56de8f3d02274fcf6b42eaff714bf4089dc
cloud4ttlcvs-spec <cloud4ttlcvs@gmail.com>
2026-02-26 10:26:28 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// sw.js - TTL Bio-Tech Admin (App Shell only) - v98.11.9
-const CACHE_NAME = 'ttl-admin-shell-v98.11.9';
+// sw.js - TTL Bio-Tech Admin (App Shell only) - v98.11.10
+const CACHE_NAME = 'ttl-admin-shell-v98.11.10';
 const CORE_ASSETS = [
   './',
   './manage_ttlbiotech_f3p4.html',
